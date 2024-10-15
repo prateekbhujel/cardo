007f6292a08ca04edc5dfa106f98de31af0c071a
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-10-15 22:34:02 +0545

    WIP - 3 Complete - Policy created BoardPolicy - Model and migration created for Board. - Component and getting thinsg ready done.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Livewire\BoardShow;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
+
+Route::get('/boards/{board}', BoardShow::class);
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
