7f4934b76accdb55f6e4f179db137042196e6d13
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-10-17 16:35:04 +0545

    [12.] Creating Column Titles

--- app/Livewire/BoardShow.php
+++ app/Livewire/BoardShow.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\Board;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
@@ -27,8 +28,30 @@ class BoardShow extends Component
     }
 
     public function moved(array $items)
-    {
-       dd($items);
+    {      
+        collect($items)->recursive()->each(function ($column) {
+           
+            $columnId =  $column->get('value');
+
+            $order = $column->get('items')->pluck('value')->toArray();
+
+            $cards = \App\Models\Card::where('user_id',  Auth::id())
+                            ->find($order)
+                            ->where('column_id', '!=', $columnId)
+                            ->each->update([
+                                'column_id'  => $columnId
+                            ]);
+
+            Log::info($cards);
+
+            \App\Models\Card::setNewOrder($order, 1, 'id', function (Builder $query) {
+                $query->where('user_id', Auth::id());
+            });
+
+            
+            // get order of cards inside that columns.
+
+      });
     }
 
     #[Layout('layouts.app')]
