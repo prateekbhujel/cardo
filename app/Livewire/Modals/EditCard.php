0dfd815f0cc119259cec79749122a2f87bc35dc6
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-10-20 17:56:44 +0545

    Modal Created for Card: - Pulled the package name : wire-elements/Modal.

---
+++ app/Livewire/Modals/EditCard.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire\Modals;
+
+use App\Models\Card;
+use LivewireUI\Modal\ModalComponent;
+
+class EditCard extends ModalComponent
+{
+    public Card $card;
+
+    public function render()
+    {
+        return view('livewire.modals.edit-card');
+    }
+}
